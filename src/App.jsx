62a53f204b7f8bf3de060107da8e17e397979302
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-02-14 09:52:55 +0530

    style: Add container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import authService from "./appwrite/auth";
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <h1>Blog APP</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between">
+      <div className="w-full block">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
