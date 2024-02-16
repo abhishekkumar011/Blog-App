1d1aa062ff964e755a36367610776cd5632c1f30
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-02-16 10:09:23 +0530

    style: Add select component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import MyButton from "./button/MyButton";
 import LogoutBtn from "./header/LogoutBtn";
 import Header from "./header/Header";
 import MyInput from "./input/MyInput"
+import Select from "./select/Select";
 
-export { Container, Footer, MyButton, LogoutBtn, Header, MyInput };
+export { Container, Footer, MyButton, LogoutBtn, Header, MyInput, Select };
