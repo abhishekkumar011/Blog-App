0a3792314b9cee32de180786cc32c2a94901b293
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-02-16 16:58:56 +0530

    rfc: refactor some style in button component

--- src/components/button/MyButton.jsx
+++ src/components/button/MyButton.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const MyButton = ({
   children,
   type = "button",
-  bgColor = "bg-green-700",
+  bgColor = "bg-primary-dark",
   textColor = "text-white",
   classname = "",
   ...props
 }) => {
   return (
     <button
-      className={`px-4 py-2 rounded-lg bg-primary-dark hover:bg-primary duration-300 ${classname} ${bgColor} ${textColor}`}
+      className={`px-4 py-2 rounded-lg hover:bg-primary duration-300 ${classname} ${bgColor} ${textColor}`}
       type={type}
       {...props}
     >
