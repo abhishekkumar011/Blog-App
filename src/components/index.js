0c1b4dee14222fb1d35021757dc3ea1522da96dd
Abhishek Kumar <abhishekkumar0336165@gmail.com>
2024-02-17 08:21:08 +0530

    feat: Add authLayout component

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,17 @@ import Footer from "./footer/Footer";
 import MyButton from "./button/MyButton";
 import LogoutBtn from "./header/LogoutBtn";
 import Header from "./header/Header";
-import MyInput from "./input/MyInput"
+import MyInput from "./input/MyInput";
 import Select from "./select/Select";
+import AuthLayout from "./authLayout/AuthLayout"
 
-export { Container, Footer, MyButton, LogoutBtn, Header, MyInput, Select };
+export {
+  Container,
+  Footer,
+  MyButton,
+  LogoutBtn,
+  Header,
+  MyInput,
+  Select,
+  AuthLayout,
+};
